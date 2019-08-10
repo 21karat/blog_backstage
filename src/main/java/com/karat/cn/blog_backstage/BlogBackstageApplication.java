219acbd700181fa8560d314deb9120650c811374
“Yokarat <1252515809@qq.com>
2019-08-10 23:28:26 +0800

    123

---
+++ src/main/java/com/karat/cn/blog_backstage/BlogBackstageApplication.java
@@ -0,0 +1,13 @@
+package com.karat.cn.blog_backstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackstageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karat/cn/blog_backstage/BlogBackstageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.karat.cn.blog_backstage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogBackstageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
